581778a27a4bc4556eb09db3c66de08efdefbb61
Thomas Hull <thull@scottlogic.co.uk>
2015-08-19 12:15:02 +0100

    Fixed 300-399 status codes no longer throw errors cuz they aint

--- public/main.js
+++ public/main.js
@@ -152,7 +152,7 @@ function findTodo(id) {
 }
 
 function checkStatus(response) {
-    if (response.status >= 200 && response.status < 300) {
+    if (response.status >= 200 && response.status < 400) {
         return response;
     } else {
         var error = new Error(response.statusText);
